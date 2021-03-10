a32d977d0ae5135373ecb03ae654c564355dda16
Abhinav Jha <abhinavjha98ald@gmail.com>
2021-03-10 22:55:47 +0530

    Added the image2text model

--- app.py
+++ app.py
@@ -13,10 +13,16 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+import datetime
+import io
+import pytesseract
+from PIL import Image
+from flask import Flask, request, render_template, redirect, url_for, session
 
 import nltk
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 Pkl_Filename = open('models/spam_detect.pkl','rb')  
 Pickled_LR_Model = pickle.load(Pkl_Filename)
 
@@ -59,5 +65,39 @@ def hello_world():
         return render_template('show.html',hspam=y_pred[0].upper(),message=message)
     return render_template('index.html')
 
+@app.route('/scanner', methods=['GET', 'POST'])
+def scan_file():
+    if request.method == 'POST':
+        start_time = datetime.datetime.now()
+        image_data = request.files['file'].read()
+        print("hello")
+        scanned_text = pytesseract.image_to_string(Image.open(io.BytesIO(image_data)))
+        print(type(scanned_text))
+        print("Found data:", scanned_text)
+        scanned_text = scanned_text.replace("\n" , " ")
+        session['data'] = {
+            "text": scanned_text,
+            "time": str((datetime.datetime.now() - start_time).total_seconds())
+        }
+
+        return redirect(url_for('result'))
+    return render_template('index.html')
+
+@app.route('/result')
+def result():
+    if "data" in session:
+        data = session['data']
+        time=data["time"],
+        text=data["text"],
+        words=len(data["text"].split(" "))
+        print(type(text))
+        messages = prepare(text)
+        print(type(messages))
+        y_pred = Pickled_LR_Model.predict(messages)
+        return render_template('show.html',hspam=y_pred[0].upper(),message=text)
+    else:
+        return "Wrong request method."
+
 if __name__ == '__main__':
-	app.run(debug=True,port=7000)
+    pytesseract.pytesseract.tesseract_cmd = r'D:\program files\Tesseract-OCR\tesseract.exe'
+    app.run(debug=True,port=7000)
